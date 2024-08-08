96fdb17661b2506f72a564f7322248da7ab199bb
CiraAdrian <98948370+CiraAdrian@users.noreply.github.com>
2024-08-08 10:00:21 +0300

    Added first file

---
+++ DebugAdi.py
@@ -0,0 +1,7 @@
+import time
+def LogAdi(text):
+    #Append-adds at last
+    file1 = open("C:/testing/Loguri.txt", "a")  # append mode
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S")  # Get the current time as a string
+    file1.write(current_time + text + "\n" )
+    file1.close()
