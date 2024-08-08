449de12cababa0aafaed52e17b3c5eb3faf5ec76
Adrian <adrian.cira@rei-d-services.com>
2024-08-08 10:44:07 +0300

    Added a comment in DebugAdi file

--- DebugAdi.py
+++ DebugAdi.py
@@ -5,3 +5,4 @@ def LogAdi(text):
     current_time = time.strftime("%Y-%m-%d %H:%M:%S")  # Get the current time as a string
     file1.write(current_time + text + "\n" )
     file1.close()
+#This is something added with first branch
